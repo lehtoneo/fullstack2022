0340b973605cb57e45dc0e911d9be9c35cee7a2b
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2022-01-11 11:29:25 +0200

    9.3

--- part9/partb/bmiCalculator.ts
+++ part9/partb/bmiCalculator.ts
@@ -1,3 +1,24 @@
+
+interface ArgumentsParsed {
+  weight: number,
+  height: number
+}
+
+const parseArgumentsBmi = (args: Array<string>): ArgumentsParsed => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      weight: Number(args[2]),
+      height: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
 const calculateBmi = (height: number, weight: number) : string => {
   const heightInMeters = height / 100
   const bmi = weight / (heightInMeters^2)
@@ -10,4 +31,13 @@ const calculateBmi = (height: number, weight: number) : string => {
   return 'Overweight'
 }
 
-console.log(calculateBmi(180, 74))
+try {
+  const { height, weight } = parseArgumentsBmi(process.argv);
+  console.log(calculateBmi(height, weight))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
--- part9/partb/exerciseCalculator.ts
+++ part9/partb/exerciseCalculator.ts
@@ -9,7 +9,33 @@ interface ExerciseResult {
 }
 type Rating = 1 | 2 | 3
 
+interface ParsedArgs {
+  hours: number[],
+  target: number
+}
 
+const parseArgumentsExercises = (args: Array<string>): ParsedArgs => {
+  if (args.length < 3) throw new Error('Not enough arguments');
+
+  const target = Number(args[2])
+  if (isNaN(target)) {
+    throw new Error('Target value is not number');
+  }
+  
+  const numberArgs = args.slice(3)
+  let hours: number[] = []
+  numberArgs.forEach(arg => {
+    if (isNaN(Number(arg))) {
+      throw new Error('Provided value is not number');
+    }
+    hours.push(Number(arg))
+  })
+  
+  return {
+    hours,
+    target
+  }
+}
 
 const getRating = (average: number, target: number) : Rating => {
   const lowerBound = target*0.8
@@ -68,4 +94,13 @@ const calculateExercises = (hours: InputArray, target: number) : ExerciseResult 
   }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+try {
+  const { hours, target }= parseArgumentsExercises(process.argv);
+  console.log(calculateExercises(hours, target))
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.'
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
