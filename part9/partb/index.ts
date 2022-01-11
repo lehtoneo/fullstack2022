877ea6ce703ea0cae38718328095eecafc1f7892
Ossi Lehtonen <ossi.j.lehtonen@helsinki.fi>
2022-01-11 12:00:55 +0200

    9.5

--- part9/partb/index.ts
+++ part9/partb/index.ts
@@ -1,10 +1,37 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import calculateBmi from './bmiCalculator';
 
 const app = express();
+
+
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack');
 });
+
+app.get('/bmi', (req: Request, res: Response) => {
+
+  const { weight, height } = req.query
+  const heightNumber = Number(height)
+  const weightNumber = Number(weight)
+  if (isNaN(weightNumber) || isNaN(heightNumber)) {
+    res.send({
+      error: 'malformatted parameters'
+    })
+    
+  } else {
+    const bmi = calculateBmi(heightNumber, weightNumber);
+
+    const result = {
+      weight,
+      height,
+      bmi
+    }
+
+    res.send(result)
+  }
+  
+})
 
 const PORT = 3003;
 
